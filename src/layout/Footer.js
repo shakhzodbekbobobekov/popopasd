1b34b07640de226079be6e4a3bcad3b5af9126d3
shaxzodbek bobobekov <shaxzodbekbobobekov@gmail.com>
2022-11-18 23:30:54 +0500

    test git

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
         <div className="container">
           © {new Date().getFullYear()} Copyright Text
           <a className="grey-text text-lighten-4 right" href="#!">
-            More Links
+            More Links s
           </a>
         </div>
       </div>
