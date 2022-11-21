1a03a5bf3f9d47f68b41f4c88a439276e8c5d347
shaxzodbek bobobekov <shaxzodbekbobobekov@gmail.com>
2022-11-21 11:41:54 +0500

    The component code that sends the request has been added

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,6 +3,22 @@ import { API_KEY, API_URL } from "./config";
 import React from "react";
 
 function Shop() {
+  const [goods, setGoods] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(API_URL, {
+      headers: {
+        Authorization: API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setGoods(data.featured) && setGoods(data.featured);
+        setLoading(false);
+      });
+  }, [goods]);
+
   return (
     <div className="content">
       <h1>Shops</h1>
